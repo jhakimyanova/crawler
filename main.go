42ab7e5333694dcc6d7ff0857c8454e140ed78e1
Iuliia Chistiakova <julia.hakimyanova@gmail.com>
2024-02-08 11:27:08 -0500

    Pagination implemented

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/jhakimyanova/crawler/scrape"
 )
 
 func main() {
-	err := scrape.Scrape("https://www.ebay.com/sch/garlandcomputer/m.html")
+	// Create a context that can be cancelled
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Listen for interrupt signal
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		// Block until a signal is received
+		sig := <-sigs
+		fmt.Println("Received signal:", sig)
+
+		// Cancel the context
+		cancel()
+	}()
+
+	err := scrape.Scrape(ctx, "https://www.ebay.com/sch/garlandcomputer/m.html")
 	if err != nil {
 		log.Fatal(err)
 	}
