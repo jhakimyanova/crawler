f2061616f8d5a0239a03848df85e4e88243b5913
Iuliia Chistiakova <julia.hakimyanova@gmail.com>
2024-02-09 12:53:28 -0500

    Added scrapper test

--- scrape/parse_html_test.go
+++ scrape/parse_html_test.go
@@ -1,10 +1,10 @@
 package scrape
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -22,14 +22,37 @@ func TestParseProductHTML(t *testing.T) {
 	defer ts.Close()
 
 	// Test scrapeTitle with the URL of the test server
-	fmt.Println(ts.URL)
 	s := Scraper{URL: ts.URL, AllowedDomain: "127.0.0.1"}
 	out := s.ScrapeProducts(ConditionAny)
-	count := 0
-	for range out {
-		count++
+
+	// Define the directory path to create
+	dirPath := filepath.Join(os.TempDir(), "testdir")
+
+	// Create the temporary directory for storing files created by this test
+	err = os.Mkdir(dirPath, 0755)
+	if err != nil {
+		t.Fatalf("Failed to create directory: %s", err)
 	}
-	if count != 25 {
-		t.Errorf("Expected %d Product items, got '%d'", 25, count)
+
+	// Defer the removal of the temporary directory
+	defer os.RemoveAll(dirPath)
+
+	SaveProductsData(dirPath, out)
+
+	// Read directory contents
+	entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		t.Fatalf("Failed to read the temporary test directory: %s", err)
+	}
+
+	// Count files (excluding directories)
+	fileCount := 0
+	for _, entry := range entries {
+		if !entry.IsDir() { // Check if the entry is not a directory
+			fileCount++
+		}
+	}
+	if fileCount != 25 {
+		t.Errorf("Expected %d product files, got '%d'", 25, fileCount)
 	}
 }
